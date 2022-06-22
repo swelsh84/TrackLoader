758ab49dfaf9e6cc22e25030213e6db7e0bd6b08
Stuart Welsh <stuartwelsh1984@hotmail.com>
2022-06-22 15:31:34 +0100

    Initial Commit

---
+++ TrackLoader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrackLoader
+//
+//  Created by Stu Welsh on 22/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrackLoader/TrackLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackLoaderApp.swift
+//  TrackLoader
+//
+//  Created by Stu Welsh on 22/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrackLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
