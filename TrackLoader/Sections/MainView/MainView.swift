9f6b85f31afcbe795a681afae236993313baeee5
Stuart Welsh <stuartwelsh1984@hotmail.com>
2022-06-22 16:39:36 +0100

    updating main view

--- TrackLoader/Sections/MainView/MainView.swift
+++ TrackLoader/Sections/MainView/MainView.swift
@@ -1,13 +1,40 @@
 import SwiftUI
 
 struct MainView: View {
+    @StateObject var viewModel: MainViewModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                switch viewModel.loadingState {
+                case .idle, .loading:
+                    ProgressView()
+                case .loaded:
+                    content
+                case .error:
+                    Text("There has been an error")
+                }
+            }
+            .navigationTitle("Rock Tracks")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .task {
+            await viewModel.fetchData()
+        }
+    }
+
+    var content: some View {
+        List {
+            ForEach(viewModel.tracks, id: \.trackId) { track in
+                Text(track.trackName)
+            }
+        }
+        .listStyle(.plain)
     }
 }
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
+        MainView(viewModel: MainViewModel(dataSource: SuccessReturningMockTracksDataSource()))
     }
 }
