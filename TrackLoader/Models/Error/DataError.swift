574f1f1f951e0d238c6fdf0eb58b4e9736f896f3
Stuart Welsh <stuartwelsh1984@hotmail.com>
2022-06-22 17:25:45 +0100

    adding livetrackdatasource with updated enums

--- TrackLoader/Models/Error/DataError.swift
+++ TrackLoader/Models/Error/DataError.swift
@@ -1,4 +1,8 @@
 enum DataError {
+    enum Decoding: Error {
+        case couldNotDecode
+    }
+
     enum JSON: Error {
         case malformedData
     }
--- TrackLoader/Models/Error/NetworkError.swift
+++ TrackLoader/Models/Error/NetworkError.swift
@@ -1,3 +1,7 @@
 enum NetworkError: Error {
     case somethingWentWrong
+    case unauthorised
+    case noResponse
+    case invalidResponse
+    case invalidUrl
 }
