28ab80669d214f1aaedea3ea21749f0f015a6f10
Stuart Welsh <stuartwelsh1984@hotmail.com>
2022-06-22 15:44:17 +0100

    adding track and track test

---
+++ TrackLoader/Models/Track.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+struct Track: Decodable {
+    var wrapperType: String
+    var kind: String
+    var artistId: Int
+    var collectionId: Int
+    var trackId: Int
+    var artistName: String
+    var collectionName: String
+    var trackName: String
+    var collectionCensoredName: String
+    var artistViewUrl: String
+    var collectionViewUrl: String
+    var trackViewUrl: String
+    var previewUrl: String
+    var artworkUrl30: String
+    var artworkUrl60: String
+    var artworkUrl100: String
+    var collectionPrice: Double
+    var trackPrice: Double
+    var releaseDate: Date
+    var collectionExplicitness: String
+    var trackExplicitness: String
+    var discCount: Int
+    var discNumber: Int
+    var trackCount: Int
+    var trackNumber: Int
+    var trackTimeMillis: Int
+    var country: String
+    var currency: String
+    var primaryGenreName: String
+    var isStreamable: Bool
+}
