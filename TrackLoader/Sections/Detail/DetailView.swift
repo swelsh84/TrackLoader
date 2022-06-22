999ffca60ac7dec9f056ae52307de9ee33c4dc8e
Stuart Welsh <stuartwelsh1984@hotmail.com>
2022-06-22 17:06:35 +0100

    updating track view

--- TrackLoader/Sections/Detail/DetailView.swift
+++ TrackLoader/Sections/Detail/DetailView.swift
@@ -1,8 +1,66 @@
-//
-//  DetailView.swift
-//  TrackLoader
-//
-//  Created by Stu Welsh on 22/06/2022.
-//
+import SwiftUI
 
-import Foundation
+struct DetailView: View {
+    @StateObject var viewModel: DetailViewModel
+
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: viewModel.track.artworkUrl60)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } placeholder: {
+                Image("Thumbnail")
+                    .aspectRatio(1.0, contentMode: .fit)
+            }
+
+            Spacer()
+
+            VStack {
+                Text(viewModel.track.trackName)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                Text(viewModel.track.artistName)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                Text("\(CurrencyFormatter.formatCurrency(currency: viewModel.track.currency, price: viewModel.track.trackPrice))")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding()
+
+            Spacer()
+
+            VStack {
+                Text("\(viewModel.track.trackTimeMillis)")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                Text("\(viewModel.track.releaseDate)")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .padding()
+
+            Spacer()
+
+            Button {
+                viewModel.openTrackUrl()
+            } label: {
+                Text("More info")
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(.blue, lineWidth: 2)
+            )
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var viewModel = DetailViewModel(track: Track.pumpedUpKicks)
+    static var previews: some View {
+        DetailView(viewModel: viewModel)
+    }
+}
