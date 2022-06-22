5b1a1fcce6128aed9c5115ce1f7809dd8a63464a
Stuart Welsh <stuartwelsh1984@hotmail.com>
2022-06-22 16:21:19 +0100

    updating mainviewmodel

--- TrackLoader/Sections/MainView/ViewModels/MainViewModel.swift
+++ TrackLoader/Sections/MainView/ViewModels/MainViewModel.swift
@@ -1,8 +1,34 @@
-//
-//  MainViewModel.swift
-//  TrackLoader
-//
-//  Created by Stu Welsh on 22/06/2022.
-//
-
+import Combine
 import Foundation
+
+@MainActor
+final class MainViewModel: ObservableObject {
+    let dataSource: TrackDataSource
+
+    @Published var loadingState = LoadingState.idle
+    @Published var tracks: [Track] = []
+
+    init(dataSource: TrackDataSource = SuccessReturningMockTracksDataSource()) {
+        self.dataSource = dataSource
+    }
+
+    func fetchData() async {
+        do {
+            let response = try await dataSource.fetchData()
+            process(response)
+        } catch {
+            handleError(error)
+        }
+    }
+
+    private func handleError(_ error: Error) {
+        self.loadingState = .error
+
+        // Here I would capture the error and report it, typically to a service like Sentry.
+    }
+
+    private func process(_ response: TrackResponse) {
+        self.loadingState = .loaded
+        self.tracks = response.results
+    }
+}
